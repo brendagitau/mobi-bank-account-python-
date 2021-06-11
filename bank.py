cec71c99018ef63fd3927093b6abce89eed0612f
brendagitau <79209524+brendagitau@users.noreply.github.com>
2021-06-11 20:23:18 +0300

    Add files via upload

--- bank.py
+++ bank.py
@@ -15,6 +15,10 @@ class Account:
         
         
     def deposit(self,amount):
+        try:
+            amount+10
+        except TypeError:
+            return f"please enter amount in digits"    
         if amount <=0:
             print ("Enter a valid amount")
         else:
@@ -26,6 +30,12 @@ class Account:
    
    
     def widthdraw(self , amount)  :
+        try:
+            amount+10
+        except TypeError:
+            return f"please enter amount in digits" 
+
+        
         total=amount+self.transactionfee
         if  self.balance== 0:
           
@@ -43,6 +53,11 @@ class Account:
     
     
     def borrow(self,amount) :
+
+        try:
+            amount+10
+        except TypeError:
+            return f"please enter amount in digits" 
            
             
         if amount<=0:
@@ -62,27 +77,85 @@ class Account:
         
 
     def repay(self,amount):
+        
+
+            
+
+            try:
+                amount+10
+            except TypeError:
+                return f" please input amount in digits"    
             if amount<0:
                 return f"You cannot repay with amount less than 0"
-            elif amount >=self.loan:
+            elif amount==self.loan:
+                return f" your loan of {self.loan} is fully paid"    
+            elif self.loan>=amount:
+                self.loan=self.loan-amount
+                transaction1={'amount':amount,'balance':self.balance,'narration':"You repaid",'time':datetime.now()}
+                self.transactions.append(transaction1)
+                return f"Your loan balance is {self.loan}"
+
+            else:
+                 
+                amount >=self.loan
                 remainder=amount-self.loan
                 self.balance+=remainder
                 transaction1={'amount':amount,'balance':self.balance,'narration':"You repaid",'time':datetime.now()}
                 self.transactions.append(transaction1)
                 return f"{amount} has been used to repay your loan,the remainder has been added to your account and balance is {self.balance}"
-            else:
-                 self.loan-=amount
-            transaction1={'amount':amount,'balance':self.balance,'narration':"You repaid",'time':datetime.now()}
-            self.transactions.append(transaction1)
-            return f"Your loan balance is {self.loan}"
 
+
+            
 
             
     def get_statement(self):
             for items in self.transactions:
-                amount=items["amount"]
+                amount=items['amount']
                 balance=items['balance']
                 narration=items["narration"]
                 time=items['time']
                 date=time.strftime("%D")
                 print(f"On {date} {narration} {amount}.Balance:{balance}")
+
+
+    def transfer(self,amount,account):
+        try:
+                amount+10
+        except TypeError:
+                return f" please input amount in digits" 
+
+        fee=amount*0.05
+        total=amount+fee
+        new_balance=self.balance-total
+        if self.balance<=0:
+            return f"You have insufficient balance in your account to complete this transaction.Your current balance is{self.balance}"
+        elif total>self.balance:
+                return f"your balance is{self.balance} you need {total} to send {amount}"
+        else:
+            self.balance-=total
+            account.deposit(amount)  
+            transaction1={'Borrow_Amount':amount,'New_balance':self.balance,'narration':"You TRANSFERED",'time':datetime.now()}
+            self.transactions.append(transaction1)
+            return f"confirmed you have transfered KES {amount} to account{account.name} .your new balance is{new_balance}"      
+
+
+class MobileMoneyAccount(Account):
+    def __init__(self, name, phone,serviceProvider):
+        Account.__init__( self,name,phone)
+        self.serviceProvider=serviceProvider
+    def buyAirtime(self,amount):
+
+        try:
+                amount+10
+        except TypeError:
+                return f" please input amount in digits" 
+        if amount<=0:
+            return f" your balance of {self.balance} below 0 .Top up & try again."
+        elif amount<=self.balance:
+            new_balance=self.balance-amount
+            transaction1={'amount':amount,'New_balance':self.balance,'narration':"You BOUGHT AIRTIME",'time':datetime.now()}
+            self.transactions.append(transaction1)
+            
+            return f"you have sucessfully bought  airtime worth {amount}.your new balance is {new_balance}"    
+
+                
